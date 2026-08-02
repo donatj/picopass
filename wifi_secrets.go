086b02c5e1000160e40908f3baf74d06bca1f1af
Jesse Donat <donatj@gmail.com>
2026-08-02 14:00:13 -0500

    Add Wi-Fi credentials placeholder

---
+++ wifi_secrets.go
@@ -0,0 +1,8 @@
+package main
+
+// Change these before flashing. This file is ignored by Git, so the password
+// is not accidentally committed if this directory becomes a repository.
+const (
+	wifiSSID     = "YOUR_WIFI_NAME"
+	wifiPassword = "YOUR_WIFI_PASSWORD"
+)
